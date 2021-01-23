f376823aa9bcd0f565809bea36ab79e849d65f74
Anton <antshekhter@gmail.com>
2021-01-23 14:17:43 -0800

    Proximity chat test

---
+++ BACA/Assets/VivoxManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VivoxUnity;
+using UnityEngine.UI;
+
+public class VivoxManager : MonoBehaviour
+{
+    VivoxVoiceManager vivox;
+    Client client = new Client();
+    [SerializeField] InputField inputUsername;
+    [SerializeField] InputField inputChannelName;
+    [SerializeField] Text textUsername;
+    [SerializeField] Text textChannelName;
+
+    void Awake() {
+        vivox = VivoxVoiceManager.Instance;
+        client.Uninitialize();
+        client.Initialize();
+        vivox.OnUserLoggedInEvent += LoggedIn;
+    }
+
+	private void OnApplicationQuit() {
+		if(vivox.LoginState == LoginState.LoggedIn) {
+            vivox.Logout();
+		}
+        client.Uninitialize();
+	}
+
+    public void Login() {
+        // filter text
+        if(!string.IsNullOrEmpty(inputUsername.text))
+            vivox.Login(inputUsername.text);
+	}
+
+    private void LoggedIn() {
+        textUsername.text = vivox.LoginSession.LoginSessionId.DisplayName;
+	}
+
+    public void Logout() {
+        if (vivox.LoginState == LoginState.LoggedIn) {
+            vivox.Logout();
+        }
+        client.Uninitialize();
+    }
+
+    public void JoinChannel() {
+        if (!string.IsNullOrEmpty(inputChannelName.text)) {
+            vivox.JoinChannel(
+                inputChannelName.text, 
+                ChannelType.NonPositional, 
+                VivoxVoiceManager.ChatCapability.AudioOnly);
+            textChannelName.text = inputChannelName.text;
+		}
+            
+    }
+}
