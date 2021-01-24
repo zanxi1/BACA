579f63bb0b35da099c7c30c900bc466855bab1af
Anton <antshekhter@gmail.com>
2021-01-23 17:01:47 -0800

    Proxchat working, starting on camera

---
+++ BACA/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+    public float turnSpeed = 4f;
+
+    void Update() {
+        transform.position = player.position + offset;
+
+    }
+
+    void LateUpdate() {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
---
+++ BACA/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+	public Transform player;
+
+	public float force = 100f;
+
+	private void FixedUpdate() {
+		if (Input.GetKey("d")) {
+			rb.AddForce(force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+		if (Input.GetKey("a")) {
+			rb.AddForce(-force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+		if (Input.GetKey("w")) {
+			rb.AddForce(0, 0, force * Time.deltaTime, ForceMode.VelocityChange);
+		}
+		if (Input.GetKey("s")) {
+			rb.AddForce(0, 0, -force * Time.deltaTime, ForceMode.VelocityChange);
+		}
+	}
+
+}
